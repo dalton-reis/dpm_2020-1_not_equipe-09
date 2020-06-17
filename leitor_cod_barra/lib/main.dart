2585c46d8f909c8910a6a2a876e6504dded50d41
Artur Bizon <artur_bizon@hotmail.com>
2020-06-17 20:30:15 -0300

    POC leitor código de barras

---
+++ leitor_cod_barra/lib/main.dart
@@ -0,0 +1,128 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ScanScreen(),
+    );
+  }
+}
+
+class ScanScreen extends StatefulWidget {
+  @override
+  _ScanState createState() => new _ScanState();
+}
+
+class _ScanState extends State<ScanScreen> {
+  String barcode = "";
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  // TODO: implement widget
+  Widget build (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text("Escaneador"),
+        centerTitle: true,
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal:16.0, vertical: 8.0),
+              child: RaisedButton(
+                color: Colors.purple,
+                textColor: Colors.white,
+                splashColor: Colors.blueGrey,
+                onPressed: scan,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.scanner, size:40,),
+                    SizedBox(height: 2,),
+                    Text("Aperte aqui para escanear")
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Text(barcode, textAlign: TextAlign.center,),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future scan() async {
+    try {
+      String baseUrl = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
+      String barcode = await BarcodeScanner.scan();
+
+      HttpClientRequest request = await HttpClient().getUrl(Uri.parse(baseUrl+barcode));
+      var response = await request.close();
+      var result = new StringBuffer();
+      await for (var contents in response.transform(Utf8Decoder())) {
+        print(contents);
+        result.write(contents);
+      }
+      Map<String,dynamic> mapResultado = jsonDecode(result.toString());
+      print(mapResultado.keys);
+      if (mapResultado["totalItems"] == 0) {
+        barcode = "Codigo de barras nao encontrado " + barcode;
+      } else {
+        Map<String,dynamic> item = mapResultado["items"][0];
+        Map<String,dynamic> livro = item["volumeInfo"];
+        print(livro.keys);
+        if (livro.containsKey("title")) {
+          if (livro.containsKey("subtitle")) {
+            barcode = livro["title"] + " - " + livro["subtitle"];
+          } else {
+            barcode = livro["title"];
+          }
+        }
+      }
+      setState(() {
+        this.barcode = barcode;
+      });
+    } on PlatformException catch(e) {
+      if (e.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          this.barcode = "Acesso da camera negado";
+        });
+      } else {
+        setState(() {
+          this.barcode = "Erro desconhecido";
+        });
+      }
+    } on FormatException catch(e){
+      print(e);
+      setState(() {
+        this.barcode = "null (reorno do usuario antes de ter sido escaneado algo)";
+      });
+    } catch (e) {
+      setState(() {
+        this.barcode = "Erro desconhecido: $e";
+      });
+    }
+  }
+}
